de7962627f2d012df88198d9cd3d75cffe275ecc
deehrlic <47901184+deehrlic@users.noreply.github.com>
2019-10-20 20:25:14 -0700

    added curl

--- start.sh
+++ start.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-
 killall -9 node
 killall -9 ngrok
 
 node drinkServer.js&
 
-ngrok http 3000&
+ngrok http 3000
 
 sleep 2
 
 curl http://127.0.0.1:4040/api/tunnels > url.py
-
-
-
-
-
