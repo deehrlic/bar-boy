f10b52457af8f2c1da161ee83f16bcd44ae72562
Drew Ehrlich <deehrlic@ucsc.edu>
2019-10-22 23:32:17 -0700

    no change

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ killall -9 ngrok
 
 node drinkServer.js&
 
-ngrok http 3000
+ngrok http 3000&
 
 sleep 2
 
-curl http://127.0.0.1:4040/api/tunnels > url.py
+curl --ipv4 http://127.0.0.1:4040/api/tunnels > url.py
 
 sleep 2
 
