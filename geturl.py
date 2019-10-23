2eaf37e1b84b5d02ba7741396454b19b01c3a2bc
deehrlic <47901184+deehrlic@users.noreply.github.com>
2019-10-23 11:49:45 -0700

    SHORTER URL!!!!

--- geturl.py
+++ geturl.py
@@ -1,4 +1,4 @@
-import re, os, urllib.request
+import re, os, urllib.request, bitly_api
 
 with open ("url.py", "r") as myfile:
     data=myfile.readlines()
@@ -12,10 +12,21 @@ found = re.findall(pat, dat)
 
 result = found[0][13:]
 
+#apiurl = "http://tinyurl.com/api-create.php?url="
+#tinyurl = urllib.request.urlopen(apiurl + result).read()
+#dec = tinyurl.decode("utf-8")
 
-apiurl = "http://tinyurl.com/api-create.php?url="
-tinyurl = urllib.request.urlopen(apiurl + result).read()
-dec = tinyurl.decode("utf-8")
+
+
+API_USER = "deehrlic"
+API_KEY = "R_1dc14729c75e474f8177814ef5b09b38"
+
+b = bitly_api.Connection(API_USER, API_KEY)
+
+longurl = result
+response = b.shorten(uri = longurl)
+
+dec = response['url']
 
 def getDec():
     return dec
